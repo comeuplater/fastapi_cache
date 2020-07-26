fdbd747cb07ddab5cbac89fbb0eb693961260cd4
Ivan Sushkov <e^ng-xq\u[Jb~6~P>
2020-07-26 20:46:36 +0700

    Added setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
+from fastapi_cache.version import __author__, __version__
 from setuptools import find_packages, setup
 
 setup(
-    name='fastapi_cache',
-    version='1.0',
+    name='FastAPI Cache',
+    version=__version__,
+    description='FastAPI simple cache',
+    author=__author__,
+    url='https://github.com/comeuplater/fastapi_cache',
     packages=find_packages(),
+    license='MIT License',
+    keywords=[
+        'redis', 'aioredis', 'asyncio', 'fastapi', 'starlette', 'cache'
+    ],
+    install_requires=[
+        'aioredis==1.3.1',
+    ],
 )
